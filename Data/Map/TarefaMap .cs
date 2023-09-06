182b755db39c3dbbd8996d8130e2048fc7be5170
Amanda Gabriela <amandagabriela056@gmail.com>
2023-09-06 18:23:20 -0300

    Foi feito as configuraçãoes para o relacionamento entre as tabelas no banco.. migration, etc..

--- Data/Map/TarefaMap .cs
+++ Data/Map/TarefaMap .cs
@@ -10,6 +10,9 @@ namespace SistemaDeTarefas.Data.Map {
             builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Descricao).HasMaxLength(10000);
             builder.Property(x => x.Status).IsRequired();
+            builder.Property(x => x.UsuarioId);
+
+            builder.HasOne(x => x.Usuario);
         }
     }
 }
